aa8646dcda3e3dbe526becc9b7a23e4251f2d298
Praveena Prakash Gunaga <gunagapraveena@gmail.com>
2024-10-17 21:33:57 +0530

    added picture

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Carousel() {
   return (
-   <div>
-    
+   <div >
+    <figure class="figure">
+  <img src="./images/pic1.jpg" class="figure-img img-fluid rounded" alt="..." style={{display: "flex", height: "90vh" ,width:"50vh",marginTop:"10%",marginLeft: "8%"}}/>
+  <figcaption class="figure-caption text-end text-center">Shree Shantika Parameshwari Devi</figcaption>
+</figure>
    </div>
   
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary " style={{background:"$red-900"}}>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary " style={{color:"$red-900"}}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"><img src= "./images/om.jpg" width="40" height="40"  className="rounded-circle"/><b><t/>Shree Shantika Parameshwari Devi</b></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <a className="navbar-brand" href="#"><img src= "./images/om.jpg" width="40" height="40"  className="rounded-circle"/><b style={{color:"brown"}}><t/>  Shree Shantika Parameshwari Devi</b></a>
+    
+   
+      <ul className="navbar-nav justify-content-end">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
@@ -32,12 +30,9 @@ const Navbar = () => {
           <a className="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+     
     </div>
-  </div>
+ 
 </nav>
     </div>
   )
